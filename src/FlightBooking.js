e3b1896b2b1925b759cb42a3af316e2c3b1962c4
Amir Hachaichi <amir.hac@outlook.com>
2022-05-15 20:20:38 +0200

    Flight booking interface done

--- src/FlightBooking.js
+++ src/FlightBooking.js
@@ -9,39 +9,45 @@ const initialState = { mode: "twoway", startdate: "", enddate: "" };
 function reducer(state, action) {
   switch (action.type) {
     case "mode":
-      return { mode: action.payload };
+      return { ...state, mode: action.payload };
     case "startdate":
-      return { startdate: action.payload };
+      return { ...state, startdate: action.payload };
     case "enddate":
-      return { enddate: action.payload };
+      return { ...state, enddate: action.payload };
     default:
       throw new Error();
   }
 }
 
+const isValidDate = (datestr) => {
+  let indate = new Date(datestr);
+  console.log(indate);
+
+  if (indate - Date.now() > 0) {
+    console.log("Valid");
+    return true;
+  } else {
+    console.log("Not Valid");
+    return false;
+  }
+};
+
 export default function FlightBooking() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const isValidDate = (event) => {
-    let indate = new Date(event.target.value);
-
-    if (indate - Date.now() > 0) {
-      console.log("Valid");
-      return true;
-    } else {
-      console.log("Not Valid");
-      return false;
-    }
-  };
-
   const changeMode = (event) => {
     dispatch({ type: "mode", payload: event.target.value });
+    if (event.target.value === "oneway") {
+      dispatch({ type: "enddate", payload: "" });
+    }
     console.log(event.target.value);
+    //endIsValid = isValidDate(event.target.value);
   };
 
   const changeStartDate = (event) => {
     dispatch({ type: "startdate", payload: event.target.value });
     console.log(state.startdate);
+    //startIsValid = isValidDate(event.target.value);
   };
 
   const changeEndDate = (event) => {
@@ -49,26 +55,58 @@ export default function FlightBooking() {
     console.log(state.enddate);
   };
 
+  const greenLight = () => {
+    if (state.mode === "oneway") {
+      return isValidDate(state.startdate);
+    } else {
+      return (
+        isValidDate(state.startdate) &&
+        isValidDate(state.enddate) &&
+        Date.parse(state.startdate) < Date.parse(state.enddate)
+      );
+    }
+  };
+
   return (
     <div className="App">
-      <div>
+      <div style={{ padding: "10px" }}>
         <select value={state.mode} onChange={changeMode}>
           <option value="twoway">Round trip</option>
           <option value="oneway">One way</option>
         </select>
       </div>
 
-      <div>
-        Departure: <input type="date" onChange={changeStartDate} />
+      <div style={{ padding: "10px" }}>
+        Departure:{" "}
+        <input
+          type="date"
+          style={
+            isValidDate(state.startdate) ? {} : { border: "2px solid red" }
+          }
+          onChange={changeStartDate}
+        />
       </div>
 
-      {state.mode != "oneway" ? (
-        <div>
-          Return: <input type="date" onChange={changeEndDate} />
+      {state.mode !== "oneway" ? (
+        <div style={{ padding: "10px" }}>
+          Return:{" "}
+          <input
+            type="date"
+            style={
+              isValidDate(state.enddate) ? {} : { border: "2px solid red" }
+            }
+            onChange={changeEndDate}
+          />
         </div>
       ) : (
         " "
       )}
+
+      <div style={{ padding: "10px" }}>
+        <button disabled={greenLight() ? false : true} onClick={() => {}}>
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
